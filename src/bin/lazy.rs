a2f081ff4b55bd806718ebda4348621142b13ff1
amazing-hash <amazing-hash@yandex.ru>
2024-07-24 21:00:22 +0400

    feat: examples lazy static

---
+++ src/bin/lazy.rs
@@ -0,0 +1,51 @@
+use std::sync::atomic::Ordering::Relaxed;
+use std::{
+    collections::HashMap,
+    sync::atomic::AtomicU64,
+    sync::{Once, OnceLock},
+};
+
+static mut VALUE: usize = 0;
+static INIT: Once = Once::new();
+
+fn get_value() -> usize {
+    unsafe {
+        INIT.call_once(|| {
+            VALUE = 1;
+        });
+        VALUE
+    }
+}
+
+fn get_value_2() -> u64 {
+    static VALUE: AtomicU64 = AtomicU64::new(0);
+    let mut value = VALUE.load(Relaxed);
+    if value == 0 {
+        value = 1;
+        VALUE.store(value, Relaxed);
+    }
+    value
+}
+
+fn get_dict() -> &'static HashMap<i32, i32> {
+    static MAP: OnceLock<HashMap<i32, i32>> = OnceLock::new();
+    MAP.get_or_init(|| {
+        let mut map = HashMap::new();
+        map.insert(1, 1);
+        map.insert(2, 2);
+        map
+    })
+}
+
+fn main() {
+    let th1 = std::thread::spawn(|| {
+        let map = get_dict();
+        println!("{} {} {}", map.get(&1).unwrap(), get_value(), get_value_2());
+    });
+    let th2 = std::thread::spawn(|| {
+        let map = get_dict();
+        println!("{} {} {}", map.get(&1).unwrap(), get_value(), get_value_2());
+    });
+    th1.join().unwrap();
+    th2.join().unwrap();
+}
