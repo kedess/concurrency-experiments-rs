5a596d4d98f8f4738dc5eadfb232de4bf91fa250
amazing-hash <amazing-hash@yandex.ru>
2024-07-25 12:44:15 +0400

    feat: improve atomic lazy init

--- src/bin/lazy.rs
+++ src/bin/lazy.rs
@@ -17,14 +17,18 @@ fn get_value() -> usize {
     }
 }
 
-fn get_value_2() -> u64 {
+fn get_value_atomic() -> u64 {
     static VALUE: AtomicU64 = AtomicU64::new(0);
     let mut value = VALUE.load(Relaxed);
     if value == 0 {
         value = 1;
-        VALUE.store(value, Relaxed);
+        match VALUE.compare_exchange(0, value, Relaxed, Relaxed) {
+            Ok(_) => value,
+            Err(v) => v,
+        }
+    } else {
+        value
     }
-    value
 }
 
 fn get_dict() -> &'static HashMap<i32, i32> {
@@ -40,11 +44,21 @@ fn get_dict() -> &'static HashMap<i32, i32> {
 fn main() {
     let th1 = std::thread::spawn(|| {
         let map = get_dict();
-        println!("{} {} {}", map.get(&1).unwrap(), get_value(), get_value_2());
+        println!(
+            "{} {} {}",
+            map.get(&1).unwrap(),
+            get_value(),
+            get_value_atomic()
+        );
     });
     let th2 = std::thread::spawn(|| {
         let map = get_dict();
-        println!("{} {} {}", map.get(&1).unwrap(), get_value(), get_value_2());
+        println!(
+            "{} {} {}",
+            map.get(&1).unwrap(),
+            get_value(),
+            get_value_atomic()
+        );
     });
     th1.join().unwrap();
     th2.join().unwrap();
