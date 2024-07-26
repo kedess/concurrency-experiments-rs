710bdbdebc04a01c02d9723d977ffab041e1de85
amazing-hash <amazing-hash@yandex.ru>
2024-07-26 15:53:51 +0400

    feat: yet another example for lazy init

--- src/bin/lazy.rs
+++ src/bin/lazy.rs
@@ -1,4 +1,5 @@
-use std::sync::atomic::Ordering::Relaxed;
+use std::sync::atomic::AtomicPtr;
+use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
 use std::{
     collections::HashMap,
     sync::atomic::AtomicU64,
@@ -8,7 +9,7 @@ use std::{
 static mut VALUE: usize = 0;
 static INIT: Once = Once::new();
 
-fn get_value() -> usize {
+fn lazy_init_value() -> usize {
     unsafe {
         INIT.call_once(|| {
             VALUE = 1;
@@ -17,7 +18,7 @@ fn get_value() -> usize {
     }
 }
 
-fn get_value_atomic() -> u64 {
+fn lazy_init_value_atomic() -> u64 {
     static VALUE: AtomicU64 = AtomicU64::new(0);
     let mut value = VALUE.load(Relaxed);
     if value == 0 {
@@ -31,7 +32,7 @@ fn get_value_atomic() -> u64 {
     }
 }
 
-fn get_dict() -> &'static HashMap<i32, i32> {
+fn lazy_init_ref() -> &'static HashMap<i32, i32> {
     static MAP: OnceLock<HashMap<i32, i32>> = OnceLock::new();
     MAP.get_or_init(|| {
         let mut map = HashMap::new();
@@ -41,23 +42,43 @@ fn get_dict() -> &'static HashMap<i32, i32> {
     })
 }
 
+fn lazy_init_ref_atomic() -> &'static HashMap<i32, i32> {
+    static PTR: AtomicPtr<HashMap<i32, i32>> = AtomicPtr::new(std::ptr::null_mut());
+    let mut ptr = PTR.load(Acquire);
+    if ptr.is_null() {
+        let mut dict = HashMap::new();
+        dict.insert(1, 1);
+        dict.insert(2, 2);
+        ptr = Box::into_raw(Box::new(dict));
+        if let Err(e) = PTR.compare_exchange(std::ptr::null_mut(), ptr, Release, Acquire) {
+            drop(unsafe { Box::from_raw(ptr) });
+            ptr = e;
+        }
+    }
+    unsafe { &*ptr }
+}
+
 fn main() {
     let th1 = std::thread::spawn(|| {
-        let map = get_dict();
+        let map = lazy_init_ref();
+        let map2 = lazy_init_ref_atomic();
         println!(
-            "{} {} {}",
+            "{} {} {} {}",
             map.get(&1).unwrap(),
-            get_value(),
-            get_value_atomic()
+            map2.get(&1).unwrap(),
+            lazy_init_value(),
+            lazy_init_value_atomic()
         );
     });
     let th2 = std::thread::spawn(|| {
-        let map = get_dict();
+        let map = lazy_init_ref();
+        let map2 = lazy_init_ref_atomic();
         println!(
-            "{} {} {}",
+            "{} {} {} {}",
             map.get(&1).unwrap(),
-            get_value(),
-            get_value_atomic()
+            map2.get(&1).unwrap(),
+            lazy_init_value(),
+            lazy_init_value_atomic()
         );
     });
     th1.join().unwrap();
